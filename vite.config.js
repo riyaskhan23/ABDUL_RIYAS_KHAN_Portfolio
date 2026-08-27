03c9bb6cd0a5b2ddbc7cfccc1d63f310dc2a5647
RiyasKhan <143700731+riyaskhan23@users.noreply.github.com>
2026-08-27 10:33:32 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    base: '/ABDUL_RIYAS_KHAN_Portfolio/',
+    base: 'ABDUL_RIYAS_KHAN_Portfolio',
   ],
   //base: '/ABDUL_RIYAS_KHAN_Portfolio/',
 })
